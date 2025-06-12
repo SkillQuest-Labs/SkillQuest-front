85cf7ea1d339047fd09ce1be80db387426b1d659
johannes60 <garilsenan@gmail.com>
2025-06-12 23:01:59 +0200

    chore: add utility functions and shadcn and TailwindCSS integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./shared/components/ui/button";
+
 function App() {
   return (
     <>
-      <div>We have nothing to show here yet.</div>
+      <div className="flex flex-col items-center justify-center min-h-screen gap-4">
+        <div className="text-red-500">met ca au centre de la page</div>
+        <Button>Button</Button>
+      </div>
     </>
   );
 }
