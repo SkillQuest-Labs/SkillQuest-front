63b810510192b38450594b6f82195b9212f5e64b
johannes60 <garilsenan@gmail.com>
2025-06-16 22:25:40 +0200

    trye

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-        <div className="text-red-500">met ca au centre de la page</div>
+        <div className="text-red-500"></div>
         <Button>Button</Button>
       </div>
     </>
