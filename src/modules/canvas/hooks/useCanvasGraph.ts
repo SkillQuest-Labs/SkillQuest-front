480504ae6f68b6276f71550b529ba66dd19b784c
johannes60 <garilsenan@gmail.com>
2025-07-07 20:24:24 +0200

    feat: track modified nodes on position change in useCanvasGraph hook

--- src/modules/canvas/hooks/useCanvasGraph.ts
+++ src/modules/canvas/hooks/useCanvasGraph.ts
@@ -34,9 +34,15 @@ export const useCanvasGraph = () => {
   const onNodesChange: OnNodesChange<Node<QuestNodeData | SkillNodeData>> = useCallback(
     (changes) => {
       const updateNode = applyNodeChanges<Node<QuestNodeData | SkillNodeData>>(changes, nodes);
+      changes.forEach((change) => {
+        if (change.type === "position" && change.id) {
+          markModifiedNode(change.id);
+        }
+      });
+
       setNodes(updateNode);
     },
-    [nodes, setNodes],
+    [nodes, setNodes, markModifiedNode],
   );
 
   const onEdgesChange: OnEdgesChange = useCallback(
