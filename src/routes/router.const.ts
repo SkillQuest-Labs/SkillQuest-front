3eedb4b19e7a525f697d33fcb48d9e34708cc055
Axel <axel.lanyan@acted.org>
2025-07-27 02:00:17 +0200

    feat: add routing for skill detail page

--- src/routes/router.const.ts
+++ src/routes/router.const.ts
@@ -6,6 +6,7 @@ export interface IRoute {
 export interface IRoutes {
   dashboard: IRoute;
   skills: IRoute;
+  skillDetail: IRoute;
   profil: IRoute;
   store: IRoute;
   canvas: IRoute;
@@ -20,6 +21,10 @@ export const routes: IRoutes = {
   skills: {
     path: "/dashboard/skills",
     display: "Skills",
+  },
+  skillDetail: {
+    path: "/dashboard/skills/:skillId",
+    display: "Skill Detail",
   },
   profil: {
     path: "/dashboard/profil",
