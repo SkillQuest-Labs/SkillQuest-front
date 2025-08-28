2fa1073f7bd6f6f290f484d26966a90a8c0777c3
johannes60 <garilsenan@gmail.com>
2025-08-28 20:04:42 +0200

    chore: create chart index file

---
+++ src/modules/stats/components/chart/index.ts
@@ -0,0 +1,18 @@
+// Types
+export type {
+  ChartData,
+  ExperienceMetric,
+  LevelProgression,
+  XpThreshold,
+  UserStats,
+  QuestCompletionMetric,
+  SkillRadarData,
+} from "../../types/stats.types";
+
+export { SkillExperienceChart } from "./SkillExperienceChart";
+export { LevelProgressionChart } from "./LevelProgressionChart";
+export { QuestCompletionChart } from "./QuestCompletionChart";
+export { SkillRadarChart } from "./SkillRadarChart";
+
+// Mock Data
+export * from "../../data/mock-data";
