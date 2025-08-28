f2a421fa9da67a119f50b361563e5f09d23531c3
johannes60 <garilsenan@gmail.com>
2025-08-29 00:42:51 +0200

    refactor: format UpdateSkillInput properties

--- src/shared/services/skill/api-skill.type.ts
+++ src/shared/services/skill/api-skill.type.ts
@@ -5,9 +5,9 @@ export type CreateSkillInput = Skill;
 export type CreateSkillResponse = Skill;
 
 export type UpdateSkillInput = Partial<Skill> & {
-    totalXp: number;
-    completedQuests: number;
-    averageQuestXp: number;
+  totalXp: number;
+  completedQuests: number;
+  averageQuestXp: number;
 };
 
 export type UpdateSkillResponse = Skill;
