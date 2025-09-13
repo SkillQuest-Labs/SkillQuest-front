760215f40b16bf094d5887353b75bc5ed9284a39
arounak9 <arouna.akonake@gmail.com>
2025-09-13 03:43:31 +0200

    feat(dashboard): afficher l'historique des quêtes et les stats sous le profil

--- src/pages/dashboard/DashboardUser.tsx
+++ src/pages/dashboard/DashboardUser.tsx
@@ -54,7 +54,6 @@ export const DashboardUser = () => {
 
           {/* 🔹 Mini section Stats */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
-            
             <FortniteStatCard
               title="XP Total"
               value={totalXp} // ton calcul existant
