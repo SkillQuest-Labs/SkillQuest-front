22b1578311244afd340d44d556a7b83c771eefbd
arounak9 <arouna.akonake@gmail.com>
2025-09-22 13:29:50 +0200

    feat(voicelines): mise a jour du voicelines

--- src/shared/constants/voiceLines.ts
+++ src/shared/constants/voiceLines.ts
@@ -1,5 +1,5 @@
 // src/shared/constants/voiceLines.ts
 export const ROBOT_INTRO_LINES = [
-  { id: "intro-1", src: "/voicelines/intro.mp3" },
+  { id: "intro-1", src: "/voicelines/intro2.mp3" },
   // { id: "intro-2", src: "/voicelines/intro2.mp4" },
 ];
